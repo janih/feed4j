0e0294f367c5762cba9709c0800df993deb249d1
Jani Halinen <jani.halinen@iki.fi>
2015-03-25 10:32:07 +0200

    user agent parameter for parser

--- src/it/sauronsoftware/feed4j/FeedParser.java
+++ src/it/sauronsoftware/feed4j/FeedParser.java
@@ -1,12 +1,14 @@
 package it.sauronsoftware.feed4j;
 
 import it.sauronsoftware.feed4j.bean.Feed;
-
 import java.net.URL;
-
+import java.net.URLConnection;
+import java.io.InputStream;
+import java.io.IOException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
+
 
 /**
  * The feed parser. It can parse RSS 1.0, RSS 2.0, Atom 0.3 and Atom 1.0.
@@ -29,13 +31,37 @@ public class FeedParser {
 	 *             The XML retrieved does not represents a feed whose kind is
 	 *             known by the parser.
 	 */
-	public static Feed parse(URL url) throws FeedIOException,
+	public static Feed parse(URL url) throws IOException, FeedIOException,
 			FeedXMLParseException, UnsupportedFeedException {
+		return parse(url, null);
+	}
+
+	/**
+	 * Gets the feed from an URL and parses it.
+	 * 
+	 * @param url
+	 *            The feed URL.
+	 * @param userAgent
+	 *            User agent string.
+	 * @return A Feed object containing the information extracted from the feed.
+	 * @throws FeedIOException
+	 *             I/O error during conetnts retrieving.
+	 * @throws FeedXMLParseException
+	 *             The document retrieved is not valid XML.
+	 * @throws UnsupportedFeedException
+	 *             The XML retrieved does not represents a feed whose kind is
+	 *             known by the parser.
+	 */	
+	public static Feed parse(URL url, String userAgent) throws IOException, FeedIOException, FeedXMLParseException, UnsupportedFeedException {
 		try {
-			// Esegue il parsing iniziale del documento XML.
+			URLConnection con = url.openConnection();
+			if (userAgent != null) {
+				con.addRequestProperty("User-Agent", userAgent);
+			}
+			con.connect();
+			InputStream is = con.getInputStream();
 			SAXReader saxReader = new SAXReader();
-			Document document = saxReader.read(url);
-			// Cerca il modulo di interpretazione del feed.
+			Document document = saxReader.read(is);
 			int code = FeedRecognizer.recognizeFeed(document);
 			switch (code) {
 			case FeedRecognizer.RSS_1_0:
@@ -53,5 +79,4 @@ public class FeedParser {
 			throw new FeedXMLParseException(e);
 		}
 	}
-
 }
